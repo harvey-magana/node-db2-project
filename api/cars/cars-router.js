132b506632caae4e0caa00aa35cbb851f217c83a
harvey magana <heyharv2012@gmail.com>
2021-01-31 09:07:39 -0800

    Added the post method to the router file.

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -29,3 +29,22 @@ router.get('/:id', (req, res) => {
             })
         })
 })
+
+router.post('/', (req, res) => {
+    const carData = req.body;
+    db('cars').insert(carData)
+        .then(ids => {
+            db('cars').where({ id: ids[0] })
+                .then(newCarEntry => {
+                    res.status(201).json(newCarEntry);
+                })
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: 'Failed to store data.'
+            })
+        })
+})
+
+
+module.exports = router;
